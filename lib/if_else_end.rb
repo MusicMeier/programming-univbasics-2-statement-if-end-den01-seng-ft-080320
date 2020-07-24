46b354fb449a3fb4314f97dbc3333fba09853942
Music Meier <musicm987@gmail.com>
2020-07-24 00:33:04 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,13 +1,15 @@
 # Write your solution here
 current_time = Time.now
-current_time.to_i
+current_time = current_time.to_i
+
 if current_time % 2 == 0
   puts "Even!"
 else
   puts "Odd!"
 end
 
-if current_time.even?
+i
+f current_time.even?
   puts "Even!"
 else
   puts "Odd!"
